c9d00ee95a592630bda9f040e98f153e0fb051e2
John Clary <john.clary@austintexas.gov>
2021-11-14 10:52:38 -0600

    use dev/prod pypi settings

--- setup.py
+++ setup.py
@@ -1,23 +1,55 @@
+import sys
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="pypgrest",
-    version="0.1.0",
-    author="City of Austin",
-    author_email="transportation.data@austintexas.gov",
-    description="A python client for interacting with PostgREST APIs.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cityofaustin/py-postgrest",
-    packages=setuptools.find_packages(),
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: Public Domain",
-        "Operating System :: OS Independent",
-        "Development Status :: 4 - Beta", 
-    ),
-)
+def get_env(args):
+    """If args contain `pypi-dev`, the package name will be `pypgrest-dev`. Else the
+    package name will be pypgrest."""
+    try:
+        # test if our custom arg is present and delete it so that setuptools doesn't
+        # throw an error
+        args.remove("pypi-dev")
+        return "dev"
+    except ValueError:
+        return "prod"
 
+
+def get_package_name(env):
+    if env == "dev":
+        return "pypgrest-dev"
+    else:
+        return "pypgrest"
+
+
+def build_config(env, readme="README.md"):
+    package_name = get_package_name(env)
+
+    with open(readme, "r") as fh:
+        long_description = fh.read()
+
+    return {
+        "author": "John Clary",
+        "author_email": "transportation.data@austintexas.gov",
+        "classifiers": [
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: Public Domain",
+            "Programming Language :: Python :: 3",
+        ],
+        "description": "A Python API client for interacting with PostgREST applications.",
+        "long_description": long_description,
+        "long_description_content_type": "text/markdown",
+        "install_requires": ["requests"],
+        "keywords": "postgres postgrest api api-client integration python",
+        "license": "Public Domain",
+        "name": package_name,
+        "packages": setuptools.find_packages(),
+        "url": "http://github.com/cityofaustin/pypgrest",
+        "version": "0.1.0",
+    }
+
+
+if __name__ == "__main__":
+    env = get_env(sys.argv)
+    config = build_config(env)
+    setuptools.setup(**config)
